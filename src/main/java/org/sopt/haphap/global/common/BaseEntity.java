f44d12a93209d074c6ab3c2f2fa76417c1ad24e4
byunhm02 <byunhm02@gmail.com>
2026-06-25 16:34:17 +0900

    feat: BaseEntity 주석 해제

--- src/main/java/org/sopt/haphap/global/common/BaseEntity.java
+++ src/main/java/org/sopt/haphap/global/common/BaseEntity.java
@@ -1,10 +1,14 @@
 package org.sopt.haphap.global.common;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-/*
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
@@ -18,4 +22,3 @@ public abstract class BaseEntity {
 
 }
 
- */
