8a9bb64868c67994878a85cd23631372520bdfdc
sophie-24 <gyuri5578@naver.com>
2026-06-27 13:58:11 +0900

    chore: Swagger 프로덕션 URL 추가, WebClient 타임아웃 설정, JWT_SECRET 환경변수 추가

--- src/main/java/org/sopt/haphap/global/config/WebClientConfig.java
+++ src/main/java/org/sopt/haphap/global/config/WebClientConfig.java
@@ -1,14 +1,23 @@
 package org.sopt.haphap.global.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofSeconds(5));
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
