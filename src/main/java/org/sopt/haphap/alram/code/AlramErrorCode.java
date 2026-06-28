03b9370e3b2a356e65122b513029eb5bee7bb49b
byunhm02 <byunhm02@gmail.com>
2026-06-28 14:11:05 +0900

    feat: 알람 실패 코드 구현

---
+++ src/main/java/org/sopt/haphap/alram/code/AlramErrorCode.java
@@ -0,0 +1,16 @@
+package org.sopt.haphap.alram.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.haphap.global.code.ErrorResultCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AlramErrorCode implements ErrorResultCode {
+
+    POSTING_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 공고입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
