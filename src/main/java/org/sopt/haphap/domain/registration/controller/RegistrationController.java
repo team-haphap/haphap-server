0e53d055a03024626b24722f8c88e7319b48323f
byunhm02 <byunhm02@gmail.com>
2026-06-30 16:16:42 +0900

    chore: ApiDocs 인터페이스로 분리 및 스웨거 설명 추가

--- src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationController.java
+++ src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/registrations")
-public class RegistrationController {
+public class RegistrationController implements RegistrationApiDocs {
 
     private final RegistrationService registrationService;
 
