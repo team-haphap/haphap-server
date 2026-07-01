cf2aefaff607865b459096cc9b07a47cd1fca937
sophie-24 <gyuri5578@naver.com>
2026-07-01 17:05:08 +0900

    refactor: 코드래빗 리뷰 반영

--- src/main/java/org/sopt/haphap/domain/user/controller/AuthController.java
+++ src/main/java/org/sopt/haphap/domain/user/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthApiDocs {
 
     private final AuthService authService;
 
