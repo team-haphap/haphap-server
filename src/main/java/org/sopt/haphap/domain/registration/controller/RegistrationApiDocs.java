5d9d4167954f2a8d1a736452e31843b89d5eaf10
byunhm02 <byunhm02@gmail.com>
2026-07-01 15:34:17 +0900

    feat: 등록 여부 검증 API 추가

--- src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationApiDocs.java
+++ src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationApiDocs.java
@@ -7,6 +7,7 @@ import org.sopt.haphap.domain.registration.dto.RegistrationCreateRequest;
 import org.sopt.haphap.domain.registration.dto.RegistrationCreateResponse;
 import org.sopt.haphap.global.dto.SuccessResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -22,4 +23,13 @@ public interface RegistrationApiDocs {
                 """)
     ResponseEntity<SuccessResponse<RegistrationCreateResponse>> createRegistration(@RequestHeader("X-User-Id") Long userId,
                                                                                    @Valid @RequestBody RegistrationCreateRequest request);
+
+    @Operation(summary = "등록 유효성 검증" ,
+            description = """
+                해당 공고의 동일 전형에 이미 등록하였는지 여부를 검증합니다. .
+                - '아직 몰라요' 외의 다른 데이터가 있는 경우 409 에러로 처리합니다. 
+                """)
+    ResponseEntity<SuccessResponse<Void>> check(@RequestHeader("X-Member-Id") Long userId,
+                                                @PathVariable Long postingId,
+                                                @PathVariable Long stageId);
 }
--- src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationController.java
+++ src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationController.java
@@ -2,6 +2,8 @@ package org.sopt.haphap.domain.registration.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.sopt.haphap.domain.registration.service.RegistrationCheckService;
+import org.sopt.haphap.global.code.SuccessResultCode;
 import org.sopt.haphap.global.dto.ApiResponse;
 import org.sopt.haphap.global.dto.SuccessResponse;
 import org.sopt.haphap.domain.registration.code.RegistrationSuccessCode;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class RegistrationController implements RegistrationApiDocs {
 
     private final RegistrationService registrationService;
+    private final RegistrationCheckService registrationCheckService;
 
     @PostMapping
     public ResponseEntity<SuccessResponse<RegistrationCreateResponse>> createRegistration(
@@ -34,6 +37,17 @@ public class RegistrationController implements RegistrationApiDocs {
         SuccessResponse<RegistrationCreateResponse> body =
                 ApiResponse.success(code, response);
 
+        return ResponseEntity.status(body.status()).body(body);
+    }
+
+    @GetMapping("/{postingId}/{stageId}")
+    public ResponseEntity<SuccessResponse<Void>> check(
+            @RequestHeader("X-Member-Id") Long userId,
+            @PathVariable Long postingId,
+            @PathVariable Long stageId) {
+
+        SuccessResultCode code = registrationCheckService.check(userId, postingId, stageId);
+        SuccessResponse<Void> body = ApiResponse.success(code);
         return ResponseEntity.status(body.status()).body(body);
     }
 }
