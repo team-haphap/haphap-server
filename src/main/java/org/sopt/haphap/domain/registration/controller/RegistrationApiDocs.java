9480c8748c6d994c8e53c6744e22e52bdd0b6be3
byunhm02 <byunhm02@gmail.com>
2026-07-01 16:19:37 +0900

    docs: 스웨거 설명 수정

--- src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationApiDocs.java
+++ src/main/java/org/sopt/haphap/domain/registration/controller/RegistrationApiDocs.java
@@ -17,9 +17,6 @@ public interface RegistrationApiDocs {
     @Operation(summary = "상태 등록" ,
             description = """
                 공고 . 전형 별 사용자의 상태를 등록하고 알람 여부를 설정합니다.
-                - force값은 필수 값이 아닙니다. 
-                - 기존 공고,전형에 '아직 몰라요'에 대한 데이터가 있고, 사용자가 업데이트를 원할 경우 force= true 로 설정합니다.
-                - '아직 몰라요' 외의 다른 데이터가 있는 경우 409 에러로 처리합니다. 
                 """)
     ResponseEntity<SuccessResponse<RegistrationCreateResponse>> createRegistration(@RequestHeader("X-User-Id") Long userId,
                                                                                    @Valid @RequestBody RegistrationCreateRequest request);
