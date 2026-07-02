0259f2c9d3f6c01f574ce06d2b106c5495e1cf4c
byunhm02 <byunhm02@gmail.com>
2026-07-02 19:30:17 +0900

    chore: 성공 코드 추가

--- src/main/java/org/sopt/haphap/domain/posting/code/PostingSuccessCode.java
+++ src/main/java/org/sopt/haphap/domain/posting/code/PostingSuccessCode.java
@@ -11,7 +11,8 @@ public enum PostingSuccessCode implements SuccessResultCode {
 
     POSTING_LIST_FETCHED(HttpStatus.OK, "공고명 목록 조회에 성공했습니다."),
     POSTING_STAGE_LIST_FETCHED(HttpStatus.OK, "공고별 전형 단계 조회에 성공했습니다."),
-    POPULAR_POSTINGS_FETCHED(HttpStatus.OK, "최근 결과가 올라온 공고 전체 조회에 성공했습니다.");
+    POPULAR_POSTINGS_FETCHED(HttpStatus.OK, "최근 결과가 올라온 공고 전체 조회에 성공했습니다."),
+    POSTING_ALL_LIST_FETCHED(HttpStatus.OK,"공고 전체 조회에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
