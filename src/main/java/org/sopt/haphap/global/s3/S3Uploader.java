e8e3a0c553e0ba113ea2a5573d0d5013d1bdbfb4
sophie-24 <gyuri5578@naver.com>
2026-07-06 15:06:07 +0900

    feat: S3 이미지 업로드/삭제 로직 구현

--- src/main/java/org/sopt/haphap/global/s3/S3Uploader.java
+++ src/main/java/org/sopt/haphap/global/s3/S3Uploader.java
@@ -1,4 +1,52 @@
 package org.sopt.haphap.global.s3;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
 public class S3Uploader {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    public String upload(MultipartFile file, String dirName) {
+        String key = dirName + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        try {
+            s3Client.putObject(
+                    PutObjectRequest.builder()
+                            .bucket(bucket)
+                            .key(key)
+                            .contentType(file.getContentType())
+                            .build(),
+                    RequestBody.fromInputStream(file.getInputStream(), file.getSize())
+            );
+        } catch (IOException e) {
+            throw new IllegalStateException("이미지 업로드 실패", e);
+        }
+
+        return "https://%s.s3.%s.amazonaws.com/%s".formatted(bucket, region, key);
+    }
+
+    public void delete(String key) {
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build());
+    }
 }
