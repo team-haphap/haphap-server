8f006da803cf3772cbf71d065834dd8e5f88f4d0
byunhm02 <byunhm02@gmail.com>
2026-07-07 13:58:41 +0900

    feat: 성공 및 에러코드 정의

--- src/main/java/org/sopt/haphap/domain/posting/code/PostingSuccessCode.java
+++ src/main/java/org/sopt/haphap/domain/posting/code/PostingSuccessCode.java
@@ -15,7 +15,8 @@ public enum PostingSuccessCode implements SuccessResultCode {
     POSTING_ALL_LIST_FETCHED(HttpStatus.OK,"공고 전체 조회에 성공했습니다."),
     TODAY_ANNOUNCEMENT_POSTING_FETCHED(HttpStatus.OK,  "오늘 발표 예상 공고 조회에 성공했습니다."),
     POSTING_DETAIL_FETCHED(HttpStatus.OK,"공고 상세 조회에 성공했습니다."),
-    POSTING_STAGE_STATUS_FETCHED(HttpStatus.OK,"공고 전형 상태 조회에 성공했습니다.");
+    POSTING_STAGE_STATUS_FETCHED(HttpStatus.OK,"공고 전형 상태 조회에 성공했습니다."),
+    POSTING_STAGE_STATISTIC_FETCHED(HttpStatus.OK,"공고 전형별 집계 조회에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
