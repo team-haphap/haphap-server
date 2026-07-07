1b7b5cebef91984a7ca51fa2df75ae7cbe65c1c7
sophie-24 <gyuri5578@naver.com>
2026-07-07 14:37:44 +0900

    fix: CI 에러 수정

--- src/main/java/org/sopt/haphap/domain/posting/code/PostingSuccessCode.java
+++ src/main/java/org/sopt/haphap/domain/posting/code/PostingSuccessCode.java
@@ -13,7 +13,8 @@ public enum PostingSuccessCode implements SuccessResultCode {
     POSTING_STAGE_LIST_FETCHED(HttpStatus.OK, "공고별 전형 단계 조회에 성공했습니다."),
     POPULAR_POSTINGS_FETCHED(HttpStatus.OK, "최근 결과가 올라온 공고 전체 조회에 성공했습니다."),
     POSTING_ALL_LIST_FETCHED(HttpStatus.OK,"공고 전체 조회에 성공했습니다."),
-    TODAY_ANNOUNCEMENT_POSTING_FETCHED(HttpStatus.OK,  "오늘 발표 예상 공고 조회에 성공했습니다.");
+    TODAY_ANNOUNCEMENT_POSTING_FETCHED(HttpStatus.OK,  "오늘 발표 예상 공고 조회에 성공했습니다."),
+    POSTING_DETAIL_FETCHED(HttpStatus.OK, "공고 상세 조회에 성공했습니다.");
 
     private final HttpStatus status;
     private final String message;
