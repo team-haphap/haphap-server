47ceef4dfbb516c5b67eae05b12dbae46dff020a
sophie-24 <gyuri5578@naver.com>
2026-07-08 03:03:33 +0900

    fix: 에러 수정

--- src/main/java/org/sopt/haphap/domain/alram/controller/PushTokenController.java
+++ src/main/java/org/sopt/haphap/domain/alram/controller/PushTokenController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/alrams/device")
