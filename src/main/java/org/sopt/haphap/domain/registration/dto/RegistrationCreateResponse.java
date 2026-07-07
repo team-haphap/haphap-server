52f74c9155c9334a5f12ebb8bb328b848cd1c97e
byunhm02 <byunhm02@gmail.com>
2026-07-07 10:52:56 +0900

    feat: 응답에 card추가

--- src/main/java/org/sopt/haphap/domain/registration/dto/RegistrationCreateResponse.java
+++ src/main/java/org/sopt/haphap/domain/registration/dto/RegistrationCreateResponse.java
@@ -1,9 +1,15 @@
 package org.sopt.haphap.domain.registration.dto;
 
+import org.sopt.haphap.domain.registration.domain.Registration;
+
 public record RegistrationCreateResponse(
-        Long registrationId
+        Long registrationId,
+        PassCardResponse card    // PASS가 아니면 null
 ) {
-    public static RegistrationCreateResponse from(Long id) {
-        return new RegistrationCreateResponse(id);
+    public static RegistrationCreateResponse from(Registration registration) {
+        PassCardResponse card = registration.isPass()
+                ? PassCardResponse.from(registration)
+                : null;
+        return new RegistrationCreateResponse(registration.getId(), card);
     }
 }
