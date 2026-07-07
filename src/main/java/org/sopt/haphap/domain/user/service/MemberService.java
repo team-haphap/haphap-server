ca0a7911044c11e94b9cbb01dab770b900eeeb31
sophie-24 <gyuri5578@naver.com>
2026-07-08 02:54:03 +0900

    refactor: 회원 조회 API 사용자 식별 방식 변경

--- src/main/java/org/sopt/haphap/domain/user/service/MemberService.java
+++ src/main/java/org/sopt/haphap/domain/user/service/MemberService.java
@@ -3,8 +3,6 @@ package org.sopt.haphap.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.sopt.haphap.domain.user.dto.MemberResponse;
 import org.sopt.haphap.domain.user.entity.User;
-import org.sopt.haphap.global.code.AuthErrorCode;
-import org.sopt.haphap.global.exception.CustomException;
 import org.sopt.haphap.global.jwt.JwtProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final UserService userService;
-    private final JwtProvider jwtProvider;
 
-    public MemberResponse getMyInfo(String accessToken) {
-        if (!jwtProvider.validateAccessToken(accessToken)) {
-            throw new CustomException(AuthErrorCode.INVALID_ACCESS_TOKEN);
-        }
-        Long userId = jwtProvider.getUserId(accessToken);
+    public MemberResponse getMyInfo(Long userId) {
         User user = userService.findById(userId);
         return MemberResponse.from(user);
     }
