57d7c11e5955524c6bffabaef22a6657914b7668
sophie-24 <gyuri5578@naver.com>
2026-07-07 21:19:51 +0900

    fix: probability 중복 제거

--- src/main/java/org/sopt/haphap/domain/calendar/dto/CalendarDateIndicatorResponse.java
+++ src/main/java/org/sopt/haphap/domain/calendar/dto/CalendarDateIndicatorResponse.java
@@ -4,8 +4,8 @@ import org.sopt.haphap.domain.posting.domain.AnnouncementLikelihood;
 
 import java.time.LocalDate;
 
-public record CalendarDateIndicatorResponse(LocalDate date, AnnouncementLikelihood probability) {
-    public static CalendarDateIndicatorResponse of(LocalDate date, AnnouncementLikelihood probability) {
-        return new CalendarDateIndicatorResponse(date, probability);
+public record CalendarDateIndicatorResponse(LocalDate date, AnnouncementLikelihood likelihood) {
+    public static CalendarDateIndicatorResponse of(LocalDate date, AnnouncementLikelihood likelihood) {
+        return new CalendarDateIndicatorResponse(date, likelihood);
     }
 }
