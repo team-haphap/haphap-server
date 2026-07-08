4bedd7ccf399529ea94b34aaa1161a9805f1368c
sophie-24 <gyuri5578@naver.com>
2026-07-08 16:37:00 +0900

    fix: 되돌리기

--- src/main/java/org/sopt/haphap/domain/admin/dto/AdminAuthResponse.java
+++
@@ -1,3 +0,0 @@
-package org.sopt.haphap.domain.admin.dto;
-
-public record AdminAuthResponse(String accessToken, String name) {}
--- src/main/java/org/sopt/haphap/domain/admin/dto/AdminLoginRequest.java
+++
@@ -1,5 +0,0 @@
-package org.sopt.haphap.domain.admin.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AdminLoginRequest(@NotBlank String loginId, @NotBlank String password) {}
