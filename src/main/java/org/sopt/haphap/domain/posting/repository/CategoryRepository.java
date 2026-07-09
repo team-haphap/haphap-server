d4972d88eff2caf8a6715bf546fc18d3614a48fb
sophie-24 <gyuri5578@naver.com>
2026-07-09 14:13:01 +0900

    fix: 카테고리 존재 검증 메서드 추가

--- src/main/java/org/sopt/haphap/domain/posting/repository/CategoryRepository.java
+++ src/main/java/org/sopt/haphap/domain/posting/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import org.sopt.haphap.domain.posting.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName (String name);
 }
