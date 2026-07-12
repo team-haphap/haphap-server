570d4b605a29e61de20a2d75e02f3592d2f4b8b8
sophie-24 <gyuri5578@naver.com>
2026-07-13 04:56:39 +0900

    fix: 마이그레이션 추가

---
+++ src/main/resources/db/migration/V23__insert_related_search_keywords.sql
@@ -0,0 +1,6 @@
+INSERT INTO related_search_keyword (keyword, is_active, created_at, updated_at)
+VALUES
+    ('카카오', true, NOW(), NOW()),
+    ('카카오 스타일', true, NOW(), NOW()),
+    ('카카오 뱅크', true, NOW(), NOW()),
+    ('카카오 맵', true, NOW(), NOW());
