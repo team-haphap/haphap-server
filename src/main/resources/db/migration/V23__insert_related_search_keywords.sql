45d719ff659ede6c2925990f5be3f48391337dea
sophie-24 <gyuri5578@naver.com>
2026-07-13 05:46:46 +0900

    feat: 마이그레이션 수정

--- src/main/resources/db/migration/V23__insert_related_search_keywords.sql
+++ src/main/resources/db/migration/V23__insert_related_search_keywords.sql
@@ -3,4 +3,5 @@ VALUES
     ('카카오', true, NOW(), NOW()),
     ('카카오 스타일', true, NOW(), NOW()),
     ('카카오 뱅크', true, NOW(), NOW()),
-    ('카카오 맵', true, NOW(), NOW());
+    ('카카오 맵', true, NOW(), NOW())
+ON CONFLICT (keyword) DO NOTHING;
