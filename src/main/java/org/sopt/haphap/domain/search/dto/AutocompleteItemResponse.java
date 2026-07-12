1de32d92fd4567563cdfdbbb7f39b9cc74282b76
sophie-24 <gyuri5578@naver.com>
2026-07-13 05:09:02 +0900

    feat:필드 추가 (키워드, 로고 이미지)

--- src/main/java/org/sopt/haphap/domain/search/dto/AutocompleteItemResponse.java
+++ src/main/java/org/sopt/haphap/domain/search/dto/AutocompleteItemResponse.java
@@ -6,13 +6,14 @@ public record AutocompleteItemResponse(
         AutocompleteType type,
         String name,
         List<HighlightRange> highlightRanges,
-        Long postingId
+        Long postingId,
+        String imageUrl,
+        Long keywordId
 ) {
-    public static AutocompleteItemResponse company(Long id, String name, List<HighlightRange> ranges) {
-        return new AutocompleteItemResponse(AutocompleteType.COMPANY, name, ranges, id);
+    public static AutocompleteItemResponse company(Long postingId, String name, List<HighlightRange> ranges, String imageUrl) {
+        return new AutocompleteItemResponse(AutocompleteType.COMPANY, name, ranges, postingId, imageUrl, null);
     }
-
-    public static AutocompleteItemResponse job(Long id, String name, List<HighlightRange> ranges) {
-        return new AutocompleteItemResponse(AutocompleteType.KEYWORD, name, ranges, id);
+    public static AutocompleteItemResponse job(Long keywordId, String name, List<HighlightRange> ranges) {
+        return new AutocompleteItemResponse(AutocompleteType.KEYWORD, name, ranges, null, null, keywordId);
     }
 }
