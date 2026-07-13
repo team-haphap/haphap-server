6caa6439c43a204fe504123422354241e23d6135
sophie-24 <gyuri5578@naver.com>
2026-07-13 22:16:48 +0900

    feat: 자동완성 바꾸어요...

--- src/main/java/org/sopt/haphap/domain/posting/dto/PostingAutocompleteProjection.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.haphap.domain.posting.dto;
-
-public interface PostingAutocompleteProjection {
-    Long getId();
-    String getTitle();
-    String getLogoImageUrl();
-}
-
-//엔티티 전체를 가져오지 않고, 필요한 애들만 가져오기 위해서 인터페이스 프로젝션으로 받도록 구현했습니다
--- src/main/java/org/sopt/haphap/domain/posting/dto/projection/PostingAutocompleteProjection.java
+++ src/main/java/org/sopt/haphap/domain/posting/dto/projection/PostingAutocompleteProjection.java
@@ -3,5 +3,6 @@ package org.sopt.haphap.domain.posting.dto.projection;
 public interface PostingAutocompleteProjection {
     Long getId();
     String getTitle();
+    String getCompanyName();
     String getLogoImageUrl();
 }
