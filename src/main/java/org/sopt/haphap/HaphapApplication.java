ac2b592dc664dde45d2bfe9b962c9cbc50ed6935
byunhm02 <byunhm02@gmail.com>
2026-07-14 16:36:47 +0900

    hotfix: timezone 설정

--- src/main/java/org/sopt/haphap/HaphapApplication.java
+++ src/main/java/org/sopt/haphap/HaphapApplication.java
@@ -1,16 +1,21 @@
 package org.sopt.haphap;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableAsync
-//@EnableJpaAuditing
 @SpringBootApplication
 public class HaphapApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(HaphapApplication.class, args);
     }
